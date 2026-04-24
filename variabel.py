11a66d1ea25e348957907ce5543c587af823c57a
andi <matulaandi@gmail.com>
2026-04-24 20:48:00 +0800

    initial project

---
+++ variabel.py
@@ -0,0 +1,18 @@
+nama = "andi"
+petiksatu = 'contoh petik satu'
+kalimatPanjang = """ ini contoh
+kalimat panjang nya atau 
+multy baris
+"""
+umur = 26
+tinggi = 153.9
+
+empty_string = ""
+anothers_empty_string = ' '
+
+print(nama)
+print(umur)
+print(type(tinggi))
+print(type(nama))
+print(petiksatu)
+print(kalimatPanjang)

8b1eb455a57aaccbc6fcc77ebc12b66c41cce78e
andi <matulaandi@gmail.com>
2026-05-01 22:34:17 +0800

    addNewFile: if.py

---
+++ if.py
@@ -0,0 +1,16 @@
+# x = int(input("Please enter an integer: "))
+# if x < 0:
+#     x = 0
+#     print("Negative changed to zero")
+# elif x == 0:
+#     print("Zero")
+# elif x == 1:
+#     print("Single")
+# else:
+#     print("More")
+
+umur = int(input("Please enter your age: "))
+if umur >= 18:
+    print("You age is ",umur,"old" " Welcome to Game")
+elif umur < 18:
+    print("You age is",umur,"old" " You are not allowed to play")
